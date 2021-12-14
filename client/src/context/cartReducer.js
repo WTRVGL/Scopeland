18da8d3f0c89ca525e36abcbb27f4101a8d29a8c
Wouter <wtrvngl@gmail.com>
2021-12-14 20:36:55 +0100

    cartReducer updatet quantity in plaats van volledig nieuw product object toe te voegen

--- client/src/context/cartReducer.js
+++ client/src/context/cartReducer.js
@@ -1,10 +1,30 @@
 export const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
+      const nextCart = [...state.cart];
+      const existingIndex = nextCart.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (existingIndex >= 0) {
+        const newQuantity = parseInt(nextCart[existingIndex].quantity + 1);
+
+        nextCart[existingIndex] = {
+          ...action.payload,
+          quantity: newQuantity,
+        };
+      } else {
+        nextCart.push({ ...action.payload, quantity: 1 });
+      }
+
       return {
         ...state,
-        cart: [...state.cart, { ...action.payload, quantity: 1 }],
+        cart: nextCart,
       };
+    // return {
+    //   ...state,
+    //   cart: [...state.cart, item],
+    // };
     case "REMOVE_FROM_CART":
       return {
         ...state,
