33bfa44b770bf14b6d4a060acc8bdec697417f06
WTR <wtrvngl@gmail.com>
2021-12-15 08:57:24 +0100

    ADD_TO_CART action type in cartReducer herwerkt

--- client/src/context/cartReducer.js
+++ client/src/context/cartReducer.js
@@ -1,35 +1,35 @@
 export const cartReducer = (state, action) => {
+
+  const {payload: {quantity, product}} = action
+
   switch (action.type) {
     case "ADD_TO_CART":
       const nextCart = [...state.cart];
       const existingIndex = nextCart.findIndex(
-        (item) => item.id === action.payload.id
+        (item) => item.id === product.id
       );
 
       if (existingIndex >= 0) {
         const newQuantity = parseInt(nextCart[existingIndex].quantity + 1);
 
         nextCart[existingIndex] = {
-          ...action.payload,
+          ...action.payload.product,
           quantity: newQuantity,
         };
       } else {
-        nextCart.push({ ...action.payload, quantity: 1 });
+        nextCart.push({ ...product, quantity: 1 });
       }
 
       return {
         ...state,
         cart: nextCart,
       };
-    // return {
-    //   ...state,
-    //   cart: [...state.cart, item],
-    // };
+
     case "REMOVE_FROM_CART":
       return {
         ...state,
-        cart: state.cart.filter((product) => {
-          return product.id !== action.payload.id;
+        cart: state.cart.filter((pr) => {
+          return pr.id !== product.id;
         }),
       };
     default:
