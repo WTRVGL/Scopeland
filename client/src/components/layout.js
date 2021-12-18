2fca3e76ebd4428b5710e222bbb9e800cf6cc9bd
WTR <wtrvngl@gmail.com>
2021-12-18 04:58:00 +0100

    Ongebruikte imports verwijderd

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import Footer from "./footer";
 import Header from "./header";
 import ShoppingCartModal from "./shopping-cart/shoppingCartModal";
 import { CartModalState } from "../context/cartModalContext";
-import styled from "styled-components";
-import { motion } from "framer-motion";
 
 const Layout = ({ children }) => {
   const { modalVisibility, setmodalVisibility } = CartModalState();
