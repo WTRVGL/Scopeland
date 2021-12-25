0b1ae253aab64b354eb8ab36d31263492d3b98ea
Wouter <wtrvngl@gmail.com>
2021-12-25 04:05:00 +0100

    Graphql query verplaatst in variabel

--- client/src/components/filter-categories/filterByCategory.js
+++ client/src/components/filter-categories/filterByCategory.js
@@ -10,87 +10,72 @@ const FilterByCategory = () => {
   const [showCategoryList, setShowCategoryList] = useState(false);
 
   const toggleCategoryList = () => {
-    showCategoryList ? setShowCategoryList(false) : setShowCategoryList(true)
-  }
+    showCategoryList ? setShowCategoryList(false) : setShowCategoryList(true);
+  };
 
   return (
     <StaticQuery
-      query={graphql`
-      query MyQuery {
-        allCatJson {
-          edges {
-            node {
-              category
-              image {
-                childImageSharp {
-                  gatsbyImageData(height: 175 layout: CONSTRAINED)
-                }
-              }
-            }
-          }
-        }
-      }
-      
-      
-      
-      
-
-      
-      
-      `}
+      query={query}
       render={({ allCatJson: { edges } }) => (
         <>
-        <Container>
-      <Row style={{ alignItems: "center", justifyContent: "center" }}>
-        <Col lg={6}>
-          <Filter>Sorteer</Filter>
-        </Col>
-        <Col>
-          <Title onClick={toggleCategoryList}>
-            Categorie
-          </Title>
-        </Col>
-        <Col>
-          <Title>
-            Astronomie
-          </Title>
-        </Col>
-        <Col>
-          <Title>
-            Astrofotografie
-          </Title>
-        </Col>
-      </Row>
-
-        
-
-
-    </Container>
-{ showCategoryList && <FilterContainer>
-
-    {edges.map(({node: {category, image}}) => {
-      const img = getImage(image);
-      return(
-        <ProductContainer>
-          <GatsbyImage image={img}/>
-          {category}
-        </ProductContainer>
-        )
-      })}
-  </FilterContainer>}
-      </>
+          <Container>
+            <Row style={{ alignItems: "center", justifyContent: "center" }}>
+              <Col lg={6}>
+                <Filter>Sorteer</Filter>
+              </Col>
+              <Col>
+                <Title onClick={toggleCategoryList}>Categorie</Title>
+              </Col>
+              <Col>
+                <Title>Astronomie</Title>
+              </Col>
+              <Col>
+                <Title>Astrofotografie</Title>
+              </Col>
+            </Row>
+          </Container>
+          {showCategoryList && (
+            <FilterContainer>
+              {edges.map(({ node: { category, image } }) => {
+                const img = getImage(image);
+                return (
+                  <ProductContainer>
+                    <GatsbyImage image={img} />
+                    {category}
+                  </ProductContainer>
+                );
+              })}
+            </FilterContainer>
+          )}
+        </>
       )}
     />
-    
   );
 };
 
 export default FilterByCategory;
 
+const query = graphql`
+  query MyQuery {
+    allCatJson {
+      edges {
+        node {
+          category
+          image {
+            childImageSharp {
+              gatsbyImageData(height: 175, layout: CONSTRAINED)
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 const FilterContainer = styled(motion.div)`
-display: flex;
-justify-content: space-between;
-height: auto;
+  display: flex;
+  justify-content: space-between;
+  height: auto;
 `;
 
 const Filter = styled.h1`
@@ -101,6 +86,6 @@ const Filter = styled.h1`
 const Title = styled.h4``;
 
 const ProductContainer = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
