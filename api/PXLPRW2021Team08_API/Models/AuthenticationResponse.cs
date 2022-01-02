6d64cbe3801c4aad16ee20a8004b75eef6851d6e
Wouter <wtrvngl@gmail.com>
2022-01-02 01:04:30 +0100

    Authentication request + response aangemaakt

---
+++ api/PXLPRW2021Team08_API/Models/AuthenticationResponse.cs
@@ -0,0 +1,22 @@
+﻿
+namespace PXLPRW2021Team08_API.Models
+{
+    public class AuthenticateResponse
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+
+
+        public AuthenticateResponse(Gebruiker user, string token)
+        {
+            Id = user.GebruikerID;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Email = user.Email;
+            Token = token;
+        }
+    }
+}
