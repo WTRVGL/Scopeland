71bf9b170ff69a68ee09f6744452dd892d465270
Wouter <wtrvngl@gmail.com>
2022-01-02 01:06:47 +0100

    rest

---
+++ api/PXLPRW2021Team08_API/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PXLPRW2021Team08_API.Data
+{
+    public class AppDbContext : IdentityDbContext<AppDbContext>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
