c03b55cde11bb97d300d860d8a70cc352dfdab98
WTR <wtrvngl@gmail.com>
2022-01-04 09:52:16 +0100

    Wrap rootLayout met AuthProvider

--- gatsby_client/src/components/rootLayout.js
+++ gatsby_client/src/components/rootLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import CartContext from "../context/cartContext";
 import CartModalContext from "../context/cartModalContext";
+import { ProvideAuth } from "../lib/auth";
 
 const lightTheme = {
   colors: {
@@ -13,11 +14,13 @@ const lightTheme = {
 
 const RootLayout = ({ children }) => {
   return (
+    <ProvideAuth>
     <CartModalContext>
       <CartContext>
         <ThemeProvider theme={lightTheme}>{children}</ThemeProvider>
       </CartContext>
     </CartModalContext>
+    </ProvideAuth>
   );
 };
 
