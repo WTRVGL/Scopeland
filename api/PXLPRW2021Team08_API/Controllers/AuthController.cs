0ff8773224f33e92158967e21019d2b36937ad63
WTR <wtrvngl@gmail.com>
2022-01-06 19:37:13 +0100

    AuthController toegevoegd
    1 [HTTPGet] endpoint die checkt als een de request een cookie bevat die wij nodig hebben

---
+++ api/PXLPRW2021Team08_API/Controllers/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PXLPRW2021Team08_API.Models;
+using PXLPRW2021Team08_API.Repositories;
+using PXLPRW2021Team08_API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PXLPRW2021Team08_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ITokenService _tokenService;
+
+        public AuthController(IUserRepository userRepository, ITokenService tokenService)
+        {
+            _userRepository = userRepository;
+            _tokenService = tokenService;
+        }
+
+        [HttpGet]
+        public IActionResult CheckAuthentication()
+        {
+            var token = HttpContext.Request.Cookies["JWTkoek"];
+
+            if (token == null) return Ok(new Gebruiker { Email = null});
+
+            var decodedToken = _tokenService.decodeJwtSecurityToken(token);
+            var id = decodedToken.Claims.FirstOrDefault(claim => claim.Type == "id");
+
+            var user = _userRepository.GetUser(Convert.ToInt32(id.Value));
+
+
+            return Ok(user);
+        }
+    }
+}
