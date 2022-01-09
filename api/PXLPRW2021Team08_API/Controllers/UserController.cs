3c074170fa639c365d8124604d5708f19b3c7c93
Wouter <wtrvngl@gmail.com>
2022-01-09 05:44:49 +0100

    User controller aangemaakt

---
+++ api/PXLPRW2021Team08_API/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PXLPRW2021Team08_API.Models;
+using PXLPRW2021Team08_API.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PXLPRW2021Team08_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IActionResult GetAllUsers()
+        {
+            return Ok(_userRepository.GetUsers());
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = _userRepository.GetUser(id);
+            if (user.GebruikerID == 0)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Gebruiker user)
+        {
+            return Ok();
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
